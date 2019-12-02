deab94356d6fdec89619ef2b62a515f7d3b45320
Weinan Qiu <davidiamyou@gmail.com>
2019-12-02 08:24:10 -0500

    remove baseline filter, modify id filter to be resource filter

--- src/protocol/filters/id.go
+++ src/protocol/filters/id.go
@@ -1,14 +1,13 @@
 package filters
 
 import (
-	"github.com/imulab/go-scim/src/core"
 	"github.com/imulab/go-scim/src/core/prop"
 	"github.com/imulab/go-scim/src/protocol"
 	uuid "github.com/satori/go.uuid"
 )
 
-// Create a new field filter that generates a new uuid on the id field.
-func NewIDFieldFilter(order int) protocol.FieldFilter {
+// Create a new resource filter that generates a new uuid on the id field.
+func NewIDResourceFilter(order int) protocol.ResourceFilter {
 	return &idFilter{order: order}
 }
 
@@ -16,19 +15,24 @@ type idFilter struct {
 	order int
 }
 
-func (u *idFilter) Supports(attribute *core.Attribute) bool {
-	return attribute.ID() == "id"
+func (f *idFilter) Order() int {
+	return f.order
 }
 
-func (u *idFilter) Order() int {
-	return u.order
+func (f *idFilter) Filter(ctx *protocol.FilterContext, resource *prop.Resource) error {
+	idProp, err := resource.NewNavigator().FocusName("id")
+	if err != nil {
+		return err
+	}
+
+	err = idProp.Replace(uuid.NewV4().String())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (u *idFilter) Filter(ctx *protocol.FilterContext, resource *prop.Resource, property core.Property) error {
-	_, err := prop.Internal(property).Replace(uuid.NewV4().String())
-	return err
-}
-
-func (u *idFilter) FieldRef(ctx *protocol.FilterContext, resource *prop.Resource, property core.Property, refResource *prop.Resource, refProperty core.Property) error {
+func (f *idFilter) FilterRef(ctx *protocol.FilterContext, resource *prop.Resource, ref *prop.Resource) error {
 	return nil
 }
