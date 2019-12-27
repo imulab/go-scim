ce6256cf98d9b057b28db27df8f1184a10d34760
Alex Valenzuela <alejandrov@fwscience.com>
2019-12-27 09:14:04 -0700

    Modify method to look for fullPath and iterate recursively over subAttributes

--- shared/schema.go
+++ shared/schema.go
@@ -257,13 +257,16 @@ func (a *Attribute) GetAttribute(p Path, recursive bool) *Attribute {
 	}
 
 	for _, subAttr := range a.SubAttributes {
-		if strings.ToLower(subAttr.Name) == strings.ToLower(p.Base()) {
+		if (strings.ToLower(subAttr.Name) == strings.ToLower(p.Base())) || (subAttr.Assist != nil && strings.ToLower(subAttr.Assist.FullPath) == strings.ToLower(p.Base())) {
 			if recursive {
 				return subAttr.GetAttribute(p.Next(), recursive)
 			} else {
 				return subAttr
 			}
-
+		} else if subAttr.SubAttributes != nil && len(subAttr.SubAttributes) == 0 {
+			for i := 0; i < len(subAttr.SubAttributes); i++ {
+				return subAttr.SubAttributes[i].GetAttribute(p, recursive)
+			}
 		}
 	}
 
