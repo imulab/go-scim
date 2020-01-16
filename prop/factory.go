4b2dfe15d4267cc6060303eedca1215ad41789db
Weinan Qiu <davidiamyou@gmail.com>
2020-01-16 11:47:50 -0500

    - add complex property - add multiValued property - complex property test - multiValued property test

---
+++ prop/factory.go
@@ -0,0 +1,31 @@
+package prop
+
+import "github.com/elvsn/scim.go/spec"
+
+// Create a new property of any legal SCIM type
+func New(attr *spec.Attribute) Property {
+	if attr.MultiValued() {
+		return NewMulti(attr)
+	}
+
+	switch attr.Type() {
+	case spec.TypeString:
+		return NewString(attr)
+	case spec.TypeInteger:
+		return NewInteger(attr)
+	case spec.TypeDecimal:
+		return NewDecimal(attr)
+	case spec.TypeBoolean:
+		return NewBoolean(attr)
+	case spec.TypeDateTime:
+		return NewDateTime(attr)
+	case spec.TypeReference:
+		return NewReference(attr)
+	case spec.TypeBinary:
+		return NewBinary(attr)
+	case spec.TypeComplex:
+		return NewComplex(attr)
+	default:
+		panic("invalid type")
+	}
+}
