8ef479b3f9c5bc028dda06c77f993b1f3c36b786
Weinan Qiu <davidiamyou@gmail.com>
2020-01-18 11:51:11 -0500

    - migrated json package - move stock schemas and resource type definitions to top level stock package

---
+++ json/options.go
@@ -0,0 +1,58 @@
+package json
+
+import (
+	"github.com/elvsn/scim.go/prop"
+	"strings"
+)
+
+// Return options to include given attributes in JSON serialization. Supplied attributes are still
+// subject to SCIM rules for return-ability.
+func Include(attributes ...string) Options {
+	return include{attributes: attributes}
+}
+
+// Return options to exclude given attributes in JSON serialization. Supplied attributes are still
+// subject to SCIM rules for return-ability.
+func Exclude(attributes ...string) Options {
+	return exclude{attributes: attributes}
+}
+
+type Options interface {
+	apply(s *serializer, resource *prop.Resource)
+}
+
+type include struct {
+	attributes []string
+}
+
+func (i include) apply(s *serializer, resource *prop.Resource) {
+	if s.includes == nil {
+		s.includes = []string{}
+	}
+	for _, path := range i.attributes {
+		if len(path) > 0 {
+			s.includes = append(s.includes, strings.TrimPrefix(
+				strings.ToLower(path),
+				strings.ToLower(resource.ResourceType().Schema().ID()+":")),
+			)
+		}
+	}
+}
+
+type exclude struct {
+	attributes []string
+}
+
+func (e exclude) apply(s *serializer, resource *prop.Resource) {
+	if s.excludes == nil {
+		s.excludes = []string{}
+	}
+	for _, path := range e.attributes {
+		if len(path) > 0 {
+			s.excludes = append(s.excludes, strings.TrimPrefix(
+				strings.ToLower(path),
+				strings.ToLower(resource.ResourceType().Schema().ID()+":")),
+			)
+		}
+	}
+}
