8597d8e06ec4798d224ab918d48384d36d990a98
Weinan Qiu <davidiamyou@gmail.com>
2020-01-30 11:45:01 -0500

    remove original godoc.sh as it is now part of Makefile

--- godoc.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-mkdir -p /tmp/tmpgoroot/doc
-rm -rf /tmp/tmpgopath/src/github.com/imulab/go-scim
-mkdir -p /tmp/tmpgopath/src/github.com/imulab/go-scim
-tar -c --exclude='.git' --exclude='tmp' . | tar -x -C /tmp/tmpgopath/src/github.com/imulab/go-scim
-echo -e "open http://localhost:6060/pkg/github.com/imulab/go-scim\n"
-GOROOT=/tmp/tmpgoroot/ GOPATH=/tmp/tmpgopath/ godoc -http=localhost:6060
